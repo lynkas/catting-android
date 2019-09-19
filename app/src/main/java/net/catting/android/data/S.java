99de83ee6698a788bcc9de883a832f2f898f034b
Cat Moki <Cat Moki>
2019-09-19 11:26:16 +0800

    update 2

--- app/src/main/java/net/catting/android/data/S.java
+++ app/src/main/java/net/catting/android/data/S.java
@@ -2,5 +2,6 @@ package net.catting.android.data;
 
 public class S {
     public static final String API_URL = "https://catting.net/api/";
+    public static final String UPDATE_JSON = "https://catting.net/api/";
 
 }
