46c86f42860255c11678ea110edc30efb5b5f070
Cat Moki <Cat Moki>
2019-09-19 12:52:59 +0800

    update 3

--- app/src/main/java/net/catting/android/UpdateActivity.java
+++ app/src/main/java/net/catting/android/UpdateActivity.java
@@ -1,5 +1,11 @@
 package net.catting.android;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.github.javiersantos.appupdater.AppUpdater;
@@ -13,12 +19,23 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import net.catting.android.data.S;
 
+import java.io.File;
+
 public class UpdateActivity extends AppCompatActivity {
+
+    private TextView title;
+    private TextView version;
+    private TextView log;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,11 @@ public class UpdateActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         checkUpdate();
+
+        title=findViewById(R.id.update_title);
+        version=findViewById(R.id.update_version);
+        log=findViewById(R.id.update_log);
+        button=findViewById(R.id.update_button);
 
     }
 
@@ -38,6 +60,19 @@ public class UpdateActivity extends AppCompatActivity {
                 .withListener(new AppUpdaterUtils.UpdateListener() {
                     @Override
                     public void onSuccess(Update update, Boolean isUpdateAvailable) {
+
+                        if (isUpdateAvailable){
+                            title.setText(R.string.update_available);
+                            version.setText(update.getLatestVersion());
+                            log.setText(update.getReleaseNotes());
+                            button.setVisibility(View.VISIBLE);
+                            button.setText(R.string.update_now);
+                            button.setOnClickListener((view)->{
+                                new Thread(()-> download(update.getUrlToDownload().toString()));
+                            });
+                        }else {
+                            title.setText(R.string.no_update_available);
+                        }
                         Log.d("Latest Version", update.getLatestVersion());
                         Log.d("Latest Version Code", update.getLatestVersionCode().toString());
                         Log.d("Release notes", update.getReleaseNotes());
@@ -47,12 +82,59 @@ public class UpdateActivity extends AppCompatActivity {
 
                     @Override
                     public void onFailed(AppUpdaterError error) {
+
                         Log.d("AppUpdater Error", "Something went wrong");
+                        Log.d("AppUpdater Error", error.toString());
                     }
                 });
 
         appUpdaterUtils.start();
         return true;
+    }
+
+    void download(String url){
+        //get destination to update file and set Uri
+        //TODO: First I wanted to store my update .apk file on internal storage for my app but apparently android does not allow you to open and install
+        //aplication with existing package from there. So for me, alternative solution is Download directory in external storage. If there is better
+        //solution, please inform us in comment
+        String destination = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/";
+        String fileName = url.split("/")[url.split("/").length-1];
+        destination += fileName;
+        final Uri uri = Uri.parse("file://" + destination);
+
+        //Delete update file if exists
+        File file = new File(destination);
+        if (file.exists())
+            //file.delete() - test this, I think sometimes it doesnt work
+            file.delete();
+
+        //set downloadmanager
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription(this.getString(R.string.downloading_notification));
+        request.setTitle(this.getString(R.string.app_name));
+
+        //set destination
+        request.setDestinationUri(uri);
+
+        // get download service and enqueue file
+        final DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        final long downloadId = manager.enqueue(request);
+
+        //set BroadcastReceiver to install app when .apk is downloaded
+        BroadcastReceiver onComplete = new BroadcastReceiver() {
+            public void onReceive(Context ctxt, Intent intent) {
+                Intent install = new Intent(Intent.ACTION_VIEW);
+                install.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                install.setDataAndType(uri,
+                        manager.getMimeTypeForDownloadedFile(downloadId));
+                startActivity(install);
+
+                unregisterReceiver(this);
+                finish();
+            }
+        };
+        //register receiver for when .apk download is compete
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
 }
