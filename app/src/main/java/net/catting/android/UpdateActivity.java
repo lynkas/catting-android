8ccdd2a163b9bb4f6b8143a1b77816bdf2c4354d
Cat Moki <Cat Moki>
2019-09-19 11:49:31 +0800

    update test 1

--- app/src/main/java/net/catting/android/UpdateActivity.java
+++ app/src/main/java/net/catting/android/UpdateActivity.java
@@ -2,13 +2,21 @@ package net.catting.android;
 
 import android.os.Bundle;
 
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.AppUpdaterUtils;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+import com.github.javiersantos.appupdater.objects.Update;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+
+import net.catting.android.data.S;
 
 public class UpdateActivity extends AppCompatActivity {
 
@@ -18,11 +26,33 @@ public class UpdateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_update);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        checkUpdate();
 
     }
 
-//    static boolean checkUpdate(){
-//
-//    }
+    boolean checkUpdate(){
+
+        AppUpdaterUtils appUpdaterUtils = new AppUpdaterUtils(this)
+                .setUpdateFrom(UpdateFrom.JSON)
+                .setUpdateJSON(S.UPDATE_JSON)
+                .withListener(new AppUpdaterUtils.UpdateListener() {
+                    @Override
+                    public void onSuccess(Update update, Boolean isUpdateAvailable) {
+                        Log.d("Latest Version", update.getLatestVersion());
+                        Log.d("Latest Version Code", update.getLatestVersionCode().toString());
+                        Log.d("Release notes", update.getReleaseNotes());
+                        Log.d("URL", update.getUrlToDownload().toString());
+                        Log.d("Is update available?", Boolean.toString(isUpdateAvailable));
+                    }
+
+                    @Override
+                    public void onFailed(AppUpdaterError error) {
+                        Log.d("AppUpdater Error", "Something went wrong");
+                    }
+                });
+
+        appUpdaterUtils.start();
+        return true;
+    }
 
 }
